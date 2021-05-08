c604d62547e9a94b0fd7116e00f42640f0c405d4
Thomas Copeland <talan.copeland@gmail.com>
2021-05-08 21:39:26 +1000

    semantic ui best practices

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css'
 import './index.css';
 import App from './App';
 import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 unregister();
-console.log("concrete-app: version 4");
+console.log("concrete-app: version 5");
