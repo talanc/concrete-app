d8ed5261d0db676a2daa29078e578b66003dc023
Thomas Copeland <talan.copeland@gmail.com>
2021-07-17 23:03:12 +1000

    updated packages

--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 unregister();
-console.log("concrete-app: version 6");
+console.log("concrete-app: version 7");
